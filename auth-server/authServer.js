7580a17573ccd89c4038bff9ec0dabbc19705289
Ahmed Sameh <ahmedsameh200382@gmail.com>
2025-05-01 22:21:09 +0300

    Return

--- auth-server/authServer.js
+++ auth-server/authServer.js
@@ -12,7 +12,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Connected to MongoDB Atlas"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
 
-
 const app = express();
 
 const corsOptions = {
