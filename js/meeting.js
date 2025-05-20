16bb0fa59df94e9ec5642f7db117f83c09b62f5d
Ahmed Sameh <ahmedsameh200382@gmail.com>
2025-05-20 12:08:43 +0300

    Return

--- js/meeting.js
+++ js/meeting.js
@@ -55,20 +55,24 @@ document.addEventListener("DOMContentLoaded", async () => {
 async function fetchIceServers() {
   return [
     {
-      urls: [ "stun:fr-turn7.xirsys.com" ]
-}, {
-   username: "L2a-fvFXKem5bHUHPf_WEX4oi-Ixl0BHHXuz4z_7KSgyjpfxuzhcVM2Tu_DfwOTUAAAAAGgpFR1haG1lZHNhbWVoMDM=",
-   credential: "c3c10bb4-3372-11f0-a269-fadfa0afc433",
-   urls: [
-       "turn:fr-turn7.xirsys.com:80?transport=udp",
-       "turn:fr-turn7.xirsys.com:3478?transport=udp",
-       "turn:fr-turn7.xirsys.com:80?transport=tcp",
-       "turn:fr-turn7.xirsys.com:3478?transport=tcp",
-       "turns:fr-turn7.xirsys.com:443?transport=tcp",
-       "turns:fr-turn7.xirsys.com:5349?transport=tcp"
-   ]
-}];
+      urls: ["stun:fr-turn7.xirsys.com"]
+    },
+    {
+      username: "L2a-fvFXKem5bHUHPf_WEX4oi-Ixl0BHHXuz4z_7KSgyjpfxuzhcVM2Tu_DfwOTUAAAAAGgpFR1haG1lZHNhbWVoMDM=",
+      credential: "c3c10bb4-3372-11f0-a269-fadfa0afc433",
+      urls: [
+        "turn:fr-turn7.xirsys.com:80?transport=udp",
+        "turn:fr-turn7.xirsys.com:3478?transport=udp",
+        "turn:fr-turn7.xirsys.com:80?transport=tcp",
+        "turn:fr-turn7.xirsys.com:3478?transport=tcp",
+        "turns:fr-turn7.xirsys.com:443?transport=tcp",
+        "turns:fr-turn7.xirsys.com:5349?transport=tcp"
+      ]
+    }
+  ];
 }
+
+
 
 ws.onopen = async () => {
   console.log("✅ WebSocket connected!");
@@ -264,11 +268,9 @@ async function createPeer(user) {
   console.log(`🤝 Creating RTCPeerConnection for user: ${user}`);
   const iceServers = await fetchIceServers();
   console.log("🧊 ICE Servers used:", iceServers);
-const peer = new RTCPeerConnection({
-  iceServers: await fetchIceServers(),
-  iceTransportPolicy: "relay" 
-});
-
+  const peer = new RTCPeerConnection({
+    iceServers: iceServers
+  });
 
   peer.oniceconnectionstatechange = () => {
     console.log(`🔌 ICE state for ${user}:`, peer.iceConnectionState);
